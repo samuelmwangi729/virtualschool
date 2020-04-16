d89e7229707bb702b655928b0bdd3c51ecf8019b
samuel mwangi <samuelmwangi729@gmail.com>
2020-04-16 05:21:55 +0300

    final

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -22,10 +22,10 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </a>
-        @if(is_null(App\Registration::where('UniqueIdentifier','=',Auth::user()->uid)->get()[0]->Status==1))
+        {{-- @if(is_null(App\Registration::where('UniqueIdentifier','=',Auth::user()->uid)->get()[0]->Status==1))
         @else
         <span style="color:red;font-weight:bold;margin-top:20px !important">Account Not Registered</span> <a href="{{route('users.reg')}}" target="_parent" class="btn btn-danger" style="margin-top:10px">Register Here</a>
-        @endif
+        @endif --}}
         <div class="navbar-right">
             <ul class="nav navbar-nav">
                 <!-- Messages: style can be found in dropdown.less-->                <!-- User Account: style can be found in dropdown.less -->
