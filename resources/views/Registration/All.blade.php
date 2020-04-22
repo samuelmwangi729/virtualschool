ad096b268777ed12d020026e80d085bdcbe3503b
samuel mwangi <samuelmwangi729@gmail.com>
2020-04-22 20:06:05 +0300

    final

--- resources/views/Registration/All.blade.php
+++ resources/views/Registration/All.blade.php
@@ -19,7 +19,18 @@
         </div>
         @endif
         <table class="table table-hover table-striped table-condensed">
-            <thead>
+             @if(count($requests) == 0)
+             <tbody>
+                 <tr>
+                     <td colspan="6">
+                         <div class="alert alert-danger">
+                             No Requests Received. Please Check Later
+                         </div>
+                     </td>
+                 </tr>
+             @else
+             @foreach ($requests as $request)
+             <thead>
                 <tr>
                     <th>Id</th>
                     <th>TransactionId</th>
@@ -30,7 +41,6 @@
                 </tr>
             </thead>
             <tbody>
-                @foreach ($requests as $request)
                 <tr>
                     <td>{{ $request->id }}</td>
                     <td>{{ $request->TransactionId }}</td>
@@ -51,7 +61,9 @@
                     </td>
                     <td class="text-center"><a href="{{ route('transaction.edit',[$request->id]) }}" class="fa fa-edit btn btn-primary btn-xs">Edit</a>&nbsp;<a href="{{ route('transaction.approve',[$request->id]) }}" class=" fa fa-check-circle btn btn-success btn-xs">Approve</a>&nbsp;<a href="{{ route('transaction.reject',[$request->id]) }}" class="fa fa-times-circle btn btn-danger btn-xs">Reject</a>&nbsp;<a href="{{ route('transaction.delete',[$request->id]) }}" class="fa fa-trash-o btn btn-danger btn-xs">Delete</a></td>
                 </tr>
+            </tbody>
                 @endforeach
+                @endif
             </tbody>
         </table>
     </div>
