9fb3315b6c243121645d1f5e400d9bb33390dae7
samuel mwangi <samuelmwangi729@gmail.com>
2020-05-02 07:11:47 +0300

    final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -339,7 +339,7 @@
                             <div class="col-lg-12">
                                 <p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 {{-- Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="ti-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> --}}
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved <span style="color:red">virtualschool.co.ke</span> |  made with <i class="fa fa-heart" style="color:red;" aria-hidden="true"></i> by <a href="#" target="_blank">SmartSoft International</a>
+Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved <span style="color:red">virtualschool.co.ke</span> |  made with <i class="fa fa-heart" style="color:red;" aria-hidden="true"></i> by <a href="#" target="_blank">SmartSoftKenya</a>
 <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                             </div>
                         </div>
